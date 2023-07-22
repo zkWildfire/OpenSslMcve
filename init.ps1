36b5866347819c96c4a5ba48ebf6c0ea52d1d2e0
Zach Wilson <zach.wilson@wizard.dev>
2023-07-22 02:30:57 -0700

    Add CMake to initialization script.

--- init.ps1
+++ init.ps1
@@ -6,8 +6,8 @@ $ErrorActionPreference = "Stop"
 
 # Check if Git is installed
 if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
-	Write-Host "Error: Git is not installed."
-	Write-Host "Please download and install Git from: https://gitforwindows.org/"
+	Write-Error "Error: Git is not installed."
+	Write-Error "Please download and install Git from: https://gitforwindows.org/"
 	exit 1
 }
 
@@ -22,20 +22,45 @@ if (-not (Test-Path $vcpkgDirectory))
 {
 	Write-Host "Cloning vcpkg into '$vcpkgDirectory'..."
 	git clone https://github.com/microsoft/vcpkg.git $vcpkgDirectory
+
+	# Run bootstrap-vcpkg.bat
+	$bootstrapScript = Join-Path -Path $vcpkgDirectory -ChildPath "bootstrap-vcpkg.bat"
+	if (Test-Path $bootstrapScript) {
+		Write-Host "Initializing vcpkg..."
+		Invoke-Expression $bootstrapScript
+	} else {
+		Write-Error "Error: Failed to find 'bootstrap-vcpkg.bat' script in the cloned VCPKG directory."
+		exit 1
+	}
 }
 else
 {
-	Write-Host "Skipping vcpkg clone (directory already exists)."
+	Write-Host "Skipping vcpkg initialization since the folder already exists."
 }
 
-# Run bootstrap-vcpkg.bat
-$bootstrapScript = Join-Path -Path $vcpkgDirectory -ChildPath "bootstrap-vcpkg.bat"
-if (Test-Path $bootstrapScript) {
-	Write-Host "Initializing vcpkg..."
-	Invoke-Expression $bootstrapScript
-} else {
-	Write-Host "Error: Failed to find 'bootstrap-vcpkg.bat' script in the cloned VCPKG directory."
-	exit 1
+# Handle CMake setup
+$cmakeDirectory = Join-Path -Path $scriptDirectory -ChildPath "_cmake"
+$cmakeZipFolder = "cmake-3.27.0-windows-x86_64"
+$cmakeUrl = "https://github.com/Kitware/CMake/releases/download/v3.27.0/$cmakeZipFolder.zip"
+$cmakeZip = Join-Path -Path $cmakeDirectory -ChildPath "cmake.zip"
+$cmakeExe = Join-Path -Path $cmakeDirectory -ChildPath "cmake/bin/cmake.exe"
+
+# Download CMake if it doesn't exist
+if (-not (Test-Path $cmakeExe))
+{
+	Write-Host "Downloading CMake..."
+	New-Item -ItemType Directory -Path $cmakeDirectory | Out-Null
+	Invoke-WebRequest -Uri $cmakeUrl -OutFile $cmakeZip
+
+	Write-Host "Extracting CMake..."
+	Expand-Archive -Path $cmakeZip -DestinationPath $cmakeDirectory
+
+	# Rename the folder so that the version number is removed
+	Rename-Item -Path $cmakeFolder -NewName "cmake"
+}
+else
+{
+	Write-Host "Skipping CMake download since the folder already exists."
 }
 
 Write-Host "Successfully finished initializing the repository."
