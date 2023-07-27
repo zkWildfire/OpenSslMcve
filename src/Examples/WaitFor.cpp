42915233ff960365fda3363787fe2e5007efb1bb
Zach Wilson <zach.wilson@wizard.dev>
2023-07-27 14:03:54 -0700

    Update wait for example (still a work in progress).
    This code now replicates the conditions that were met in the code where
    the OpenSSL issue was first detected. However, in the original code, the
    standard library code that was called into had a comparison against an
    XMM register, which was why it was affected by this bug. With the current
    work in progress code, a comparison is made against a hardcoded constant
    instead, so its behavior does not change as a result of the bug.

--- src/Examples/WaitFor.cpp
+++ src/Examples/WaitFor.cpp
@@ -8,8 +8,18 @@
 #include "Keys.hpp"
 #include "Validator.hpp"
 
+void Wait()
+{
+	std::mutex mutex;
+	std::unique_lock<std::mutex> lock(mutex);
+	std::condition_variable cv;
+	cv.wait_for(lock, std::chrono::milliseconds(100));
+}
+
 int main()
 {
+	// Run the tests so that the status of whether the current system is
+	//   affected by the OpenSSL bug is printed
 	auto makeCipher = []()
 	{
 		return Cipher(
@@ -22,10 +32,24 @@ int main()
 	auto validator = Validator(makeCipher);
 	validator.Test();
 
-	std::mutex mutex;
-	std::unique_lock<std::mutex> lock(mutex);
-	std::condition_variable cv;
-	cv.wait_for(lock, std::chrono::milliseconds(100));
+	// Show that the call to `wait_for()` is fine without the register
+	//   corruption caused by the OpenSSL bug
+	// Note that it's fine for this to occur after the validator runs its
+	//   tests because the validator will restore the registers to the correct
+	//   state before returning
+	std::cout << "Waiting for 100ms before call to OpenSSL...\n";
+	Wait();
+	std::cout << "Finished pre-OpenSSL wait.\n\n";
+
+	// Call an affected OpenSSL method without restoring the registers
+	//   afterwards to show what can happen as a result of the bug
+	std::cout << "Waiting for 100ms after the call to OpenSSL...\n";
+	std::cout << "Note: On a system affected by the bug, this should cause "
+		<< "the executable to hang.\n";
+	auto cipher = makeCipher();
+	cipher.Encrypt("Hello, world!");
+	Wait();
+	std::cout << "Finished post-OpenSSL wait.\n\n";
 
 	return 0;
 }
